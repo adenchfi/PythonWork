b010d1fb1ffb642a78f5d0ef783c9764c9d3167b
Adam Denchfield <adenchfi@hawk.iit.edu>
2015-11-06 19:00:46 +0000

    Some lesson 1 changes.

---
+++ programs_lecture_1/multirun_direct_pi.py
@@ -0,0 +1,16 @@
+import random
+ 
+def direct_pi(N):
+    n_hits = 0
+    for i in range(N):
+        x, y = random.uniform(-1.0, 1.0), random.uniform(-1.0, 1.0)
+        if x ** 2 + y ** 2 < 1.0:
+            n_hits += 1
+    return n_hits
+ 
+n_runs = int(input("How many runs do you want? "))
+n_trials = int(input("How many trials per run? "))
+
+for run in range(n_runs):
+    print (4.0 * direct_pi(n_trials) / float(n_trials))
+
